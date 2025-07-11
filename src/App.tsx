6054be6e998b669208d0db72bd5a89ea315aefdb
JevoPatrick <jevopatrick24@gmail.com>
2025-07-11 17:16:23 +0530

    Fix App.tsx Java code syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import React from 'react';
 
-public class BoltControl {
-    public static void main(String[] args) {
-        try {
-            String boltIP = "192.168.4.2";
-            String urlStr = "http://" + boltIP + "/digitalWrite?pin=0&state=HIGH";
-
-            URL url = new URL(urlStr);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            System.out.println("Response: " + response.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+function App() {
+  return (
+    <div className="App">
+      {/* Your application's main content and components will go here */}
+      <h1>Vetary Application</h1>
+    </div>
+  );
 }
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-public class BoltSensorRead {
-    public static void main(String[] args) {
-        try {
-            String boltIP = "192.168.4.2";
-            String urlStr = "http://" + boltIP + "/analogRead?pin=A0";
-
-            URL url = new URL(urlStr);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            System.out.println("Sensor Value: " + response.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+export default App;
